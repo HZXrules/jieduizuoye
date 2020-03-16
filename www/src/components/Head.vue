c7526d6aaa1fe845d36f7c3bbeff7560932da660
HZXrules <50147759+HZXrules@users.noreply.github.com>
2020-03-16 22:20:25 +0800

    Update Head.vue

--- www/src/components/Head.vue
+++ www/src/components/Head.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
     <div style="text-align: left; " class="pageHead">
-      <!-- 开源标识 -->
-      <div style="text-align: right; ">
-        <span>
-          <a href="https://github.com/simonblowsnow/2019-ncov-vis" style="color: #fff; text-decoration:none">
-            <img style="margin-bottom: -2px;" src="../assets/github.png" width="18" /> 
-            <label style="font-size: 16px"> Github</label>
-          </a>
-        </span>
-      </div>
       <div>
         <div style="font-size: 24px">新冠肺炎疫情<label style="font-size: 22px"> · 数据分析系统</label></div>
         <div style="font-size: 20px; margin-top: 5px; color: #ddd; font-family: 'Times New Roman', Times, serif">
@@ -43,13 +34,13 @@ a, a>label { cursor: pointer }
   height: 100%;
 }
 .pageHead {
-  height: 160px; 
+  height: 160px;
   padding: 20px;
   background: url(../assets/head.png) center top / 100% no-repeat;
   color: #fff
 }
 
-.el-main { 
+.el-main {
   padding: 0px 15px 20px 15px!important;
   height: calc(100% - 200px);
   /* min-height: calc(100% - 200px); */
@@ -57,7 +48,7 @@ a, a>label { cursor: pointer }
 .el-header {
     background-color: #B3C0D1;
     color: #333;
-    line-height: 60px; 
+    line-height: 60px;
 }
 .el-footer {
     background-color: #B3C0D1;
